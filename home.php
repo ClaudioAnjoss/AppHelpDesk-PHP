5f4bfe6efb53f32fb0c78b0c8301985d2637fe29
Claudio Anjos <claudio.anjoss@outlook.com>
2022-03-18 19:45:56 -0300

    teste

--- home.php
+++ home.php
@@ -1,59 +1,77 @@
-<?php
+<!--
+  <?php
 require_once "validar_pagina.php";
 ?>
+-->
 
-<html>
+<!doctype html>
+<html lang="pt-br">
 
 <head>
-  <meta charset="utf-8" />
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="css/style.css">
+
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+
   <title>App Help Desk</title>
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-  <style>
-    .card-home {
-      padding: 30px 0 0 0;
-      width: 100%;
-      margin: 0 auto;
-    }
-  </style>
 </head>
 
 <body>
 
-  <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="home.php">
-      <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-      App Help Desk
-    </a>
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a href="logoff.php" class="nav-link">Sair</a>
-      </li>
-    </ul>
-  </nav>
+  <!--Menu-->
+  <header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img width="100px" src="img/logo.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Pagína Inicial <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Controle de Chamados</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Usuários</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
 
-  <div class="container">
-    <div class="row">
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="">
+            <img width="100px" class="img-fluid" src="img/solicitacao.png" alt="">
 
-      <div class="card-home">
-        <div class="card">
-          <div class="card-header">
-            Menu
-          </div>
-          <div class="card-body">
-            <div class="row">
-              <div class="col-6 d-flex justify-content-center">
-                <a href="abrir_chamado.php"><img src="formulario_abrir_chamado.png" width="70" height="70"></a>
-              </div>
-              <div class="col-6 d-flex justify-content-center">
-                <a href="consultar_chamado.php"><img src="formulario_consultar_chamado.png" width="70" height="70"></a>
-              </div>
+            <div id="chama" class="nick_chamados">
+              <p class="nick_chamados">Solicitações</p>
             </div>
+
           </div>
         </div>
       </div>
     </div>
+  </section>
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
